f34030cf1cb8c62720f04bd88d27b1c35a4af1c6
redevverse <deveshreddy27@gmail.com>
2021-02-18 08:49:05 -0600

    before redoing part-3

--- part-3/bp.js
+++ part-3/bp.js
@@ -1,5 +1,14 @@
 // Url for data
-
+import {
+    select,
+    csv,
+    scaleLinear,
+    max,
+    scaleBand,
+    axisLeft,
+    axisBottom,
+    format
+  } from 'd3';
 
 //fetch the promise text
 //This is the main function body
@@ -59,6 +68,65 @@ const renderChart = data => {
     
     };
 
+////////////////////////////////////////////////////////////////
+const render = data => {
+    const xValue = d => d['Acceleration'];
+    const yValue = d => d.Model;
+    const margin = { top: 50, right: 40, bottom: 77, left: 180 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const xScale = scaleLinear()
+      .domain([0, max(data, xValue)])
+      .range([0, innerWidth]);
+    
+    const yScale = scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1);
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+    
+    const xAxisTickFormat = number =>
+      format('.3s')(number)
+        .replace('G', 'B');
+    
+    const xAxis = axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+    
+    g.append('g')
+      .call(axisLeft(yScale))
+      .selectAll('.domain, .tick line')
+        .remove();
+    
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`);
+    
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 65)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabelText);
+    
+    g.selectAll('rect').data(data)
+      .enter().append('rect')
+        .attr('y', d => yScale(yValue(d)))
+        .attr('width', d => xScale(xValue(d)))
+        .attr('height', yScale.bandwidth());
+    
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text(titleText);
+  };
+  
+  
+
 
 ///////////////////////////////////////////////////////////////
 /////////////Data Loading///////////////////////////////////
@@ -89,6 +157,6 @@ AllData.forEach(element => {
 // console.log(AllData);
 
 const newData = AllData.filter(function(d,i) {return(i<5);});
-renderChart(newData);
+render(newData);
 });
 
