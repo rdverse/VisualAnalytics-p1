1b4a8e2933633c4110a01dbd1e8b755404f47e78
redevverse <deveshreddy27@gmail.com>
2021-02-18 12:25:10 -0600

    Added multiple bar plots

--- part-3/app.js
+++ part-3/app.js
@@ -1,17 +1,6 @@
 
 // const width = 1000;
 // const height = 600;
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-var svg = d3.select('#my')
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
 
 // const width = +svg.attr('width');
 // const height = +svg.attr('height');
@@ -20,17 +9,33 @@ var svg = d3.select('#my')
 /////////////Chart Renderer///////////////////////////////////
 /////////////////////////////////////////////////////////////
 // chart rendering algorithm
-const renderChart = data => {
-    
-      // Add X axis
-  var x = d3.scaleLinear()
-  .domain([0, 50])
-  .range([ 0, width]);
+const renderBarChart = (data, divname) => {
+  var margin = {top: 20, right: 30, bottom: 60, left: 120},
+  width = 400 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
+var svg = d3.select("#" + divname)
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+  
+
+
+  // Add X axis
+  var x = d3.scaleLinear()
+  .domain([0, d3.max(data, d=>d[divname])])
+  .range([0,width]);
+
+  // console.log(x);
+  
 svg.append("g")
   .attr("transform", "translate(0," + height + ")")
   .call(d3.axisBottom(x))
   .selectAll("text")
+  //translate does not work for some reason, [check later]
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
 
@@ -42,19 +47,40 @@ var y = d3.scaleBand()
   
 svg.append("g")
   .call(d3.axisLeft(y));
-    console.log(data);
+  console.log(divname);  
+  console.log(data);
+
+//     d3.select('select')
+//     .on("change", function() {
+
+//     key = this.selectedIndex;
+// console.log(key);
+//   });
 
  //Bars
  svg.selectAll("myRect")
- .data(data)
- .enter()
- .append("rect")
- .attr("x", x(0) )
- .attr("y", function(d) { return y(d.Model); })
- .attr("width", function(d) { return x(d.Acceleration); })
- .attr("height", y.bandwidth() )
- .attr("fill", "#69b3a2")
-    
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", x(0) )
+    .attr("y", function(d) { return y(d.Model); })
+    .attr("width", function(d) { return x(d[divname]); })
+    .attr("height", y.bandwidth() )
+    .attr("fill", "#69b3a2");
+        
+  svg.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", (height+(16*3)))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "16px") 
+    .style("text-decoration", "underline")
+    .style('fill', 'blue')  
+    .text(divname);
+        // .text(divname);
+
+        // .style("text-anchor", "middle")
+        // .style('fill', 'darkOrange')
+        // .style('font-size', '20px')
         // .attr('width', d => xScale(xValue(d)))
         // .attr('heigth', yScale.bandwidth());
     
@@ -90,6 +116,15 @@ const DUMMY_DATA = [
     {id: 'd2', val: 30, reg: 'Canada'},
     {id: 'd3', val: 20, reg: 'India'},
     {id: 'd4', val: 11, reg: 'China'}];
-renderChart(AllData.filter((d,i)=>{return i<6}));
+
+renderBarChart(AllData.filter((d,i)=>{return i<6}), "Acceleration");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"MPG");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"Cylinders");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"Displacement");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"Horsepower");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"Weight");
+renderBarChart(AllData.filter((d,i)=>{return i<8}),"Year");
 });
+
+
 
