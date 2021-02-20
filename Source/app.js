2c840a09b4e7b38fefff1e933c9153f85b9ba3d2
redevverse <deveshreddy27@gmail.com>
2021-02-19 18:52:17 -0600

    bar plots and css adjusted

--- Source/app.js
+++ Source/app.js
@@ -1,43 +1,43 @@
 
 
-function update(){
-  carname = svg.select("#DisplayStats");
+// function update(){
+//   carname = svg.select("#DisplayStats");
     
-  circles.enter()
-      .append("circle")
-      .attr("class", "dot");
+//   circles.enter()
+//       .append("circle")
+//       .attr("class", "dot");
   
-  circles.exit().remove();
+//   circles.exit().remove();
   
-  // "update" mode
-  circles.attr("cx", function(d) {
-          return x(d.pca1);
-      })
-      .attr("cy", function(d) {
-          return y(d.pca2);
-      })
-      .style("fill", function(d) {
-          return color(d.Origin);
-      })
-      .attr("r", 5);
+//   // "update" mode
+//   circles.attr("cx", function(d) {
+//           return x(d.pca1);
+//       })
+//       .attr("cy", function(d) {
+//           return y(d.pca2);
+//       })
+//       .style("fill", function(d) {
+//           return color(d.Origin);
+//       })
+//       .attr("r", 5);
   
-  circles.on("mouseover", function(d) {
-          d3.select(this)
-              .transition()
-              .duration(10)
-              .attr("r", 8);
-          carname.text("Name of selected car: " + d.Model);
-      })
-      .on("mouseout", function() {
-          d3.select(this)
-              .transition()
-              .duration(10)
-              .attr("r", 5);
-          carname.text("Name of selected car: ____");
+//   circles.on("mouseover", function(d) {
+//           d3.select(this)
+//               .transition()
+//               .duration(10)
+//               .attr("r", 8);
+//           carname.text("Name of selected car: " + d.Model);
+//       })
+//       .on("mouseout", function() {
+//           d3.select(this)
+//               .transition()
+//               .duration(10)
+//               .attr("r", 5);
+//           carname.text("Name of selected car: ____");
   
-      })
+//       })
 
-}
+// }
 
 
 
@@ -46,10 +46,10 @@ function update(){
 /////////////Scatter Chart Renderer///////////////////////////////////
 /////////////////////////////////////////////////////////////
 // chart rendering algorithm
-const renderScatterChart = (data) => {
+function renderScatterChart(data) {
   var margin = {top: 20, right: 30, bottom: 60, left: 40},
   width = 1000 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom,
+  height = 400 - margin.top - margin.bottom,
   radius = 3, 
   radiusZoom =12;
 
@@ -130,8 +130,16 @@ var div = d3.select("body").append("div")
         .style("text-anchor", "left")
         .attr("font-size", "12px")
         .attr("font-weight", "bold");
-        
- //points
+      
+//define colors here
+// var colorInterpolate = d3.scaleSequential()
+//         .domain([d3.min(data, d=>d.MPG), d3.max(data, d=>d.MPG)])
+//         .iterpolator(d3.interpolateBlues);
+  var colorTheme  = "Acceleration"
+  var myColor = d3.scaleSequential()
+  .domain([d3.min(data, d=>d[colorTheme]), d3.max(data, d=>d[colorTheme])])
+  .interpolator(d3.interpolateBlues);
+
  svg.selectAll("circle")
     .data(data).enter().append("circle")
     // a
@@ -139,10 +147,12 @@ var div = d3.select("body").append("div")
     .attr("cy", function(d) { return(y(d.pca2));})
     // .attr("width", function(d) { return x(d[divname]);})
     .attr("r", radius)
-    .attr("fill", "#081d58")
-    .attr("fill-opacity", 0.4)
+    .attr("fill", function(d){return(myColor(d[colorTheme]));})
+   // .attr("fill-opacity", 0.4)
     .on('pointerover', function (p, d) {
-      d3.select(this).transition()
+      
+      
+    d3.select(this).transition()
           .duration('100')
           .attr("r", radiusZoom);
       
@@ -159,7 +169,7 @@ var div = d3.select("body").append("div")
     var weight = d.Weight - bmw2002Data[0].Weight;
     var acceleration = d.Acceleration - bmw2002Data[0].Acceleration;
     var year = d.Year - bmw2002Data[0].Year;
-    var origin = d.Origin - bmw2002Data[0].Origin;
+    var origin = d.Origin;
 
 // console.log(d.Weight);
 
@@ -173,6 +183,19 @@ var div = d3.select("body").append("div")
                   " Year: " + floatFormat(year,1);
           descriptionTooltip.select("text")
       .text(textToDisplay);
+
+      var Fdata = data
+      var target = d.Model;
+      Fdata = Fdata.filter((dd,ii)=>{
+          return((dd.Model==target)||(dd.Model==bmw2002Data[0].Model));});
+      console.log(Fdata);
+      renderBarChart(Fdata, "Acceleration");
+      renderBarChart(Fdata,"MPG");
+      renderBarChart(Fdata,"Cylinders");
+      renderBarChart(Fdata,"Displacement");
+      renderBarChart(Fdata,"Horsepower");
+      renderBarChart(Fdata,"Weight");
+      renderBarChart(Fdata,"Year");
 
       // .enter()
       // .append("text")
@@ -197,7 +220,20 @@ var div = d3.select("body").append("div")
       d3.select(this).transition()
           .duration('300')
           .attr("r", radius);
-      // div.transition()
+
+    d3.selectAll("svg").remove();
+    renderScatterChart(data);
+    // d3.selectAll('myRect').transition();
+    // d3.select('#MPG').transition();
+    // d3.select('#Horsepower').transition();
+    // d3.select('#Year').remove();
+    // d3.select('#Displacement').remove();
+    // d3.select('#Weight').remove();
+    // d3.select('#Year').remove();
+    // d3.select('#Cylinders').remove();
+    
+
+    // div.transition()
       //     .duration('300')
       //     .style("opacity", 0);
   });
@@ -248,14 +284,8 @@ var div = d3.select("body").append("div")
 // svg.append(() => tooltip.node);
 
 // return svg.node();
-
-
   
-  
-  
-  
-  
-  };
+  }
 
 
 
@@ -265,12 +295,14 @@ var div = d3.select("body").append("div")
 /////////////Bar chart Renderer///////////////////////////////////
 /////////////////////////////////////////////////////////////
 // chart rendering algorithm
-const renderBarChart = (data, divname) => {
-  var margin = {top: 20, right: 30, bottom: 60, left: 120},
-  width = 300 - margin.left - margin.right,
-  height = 300 - margin.top - margin.bottom;
+function renderBarChart(data, divname) {
+  // d3.selectAll("myRect").remove();
 
-var svg = d3.select("#" + divname)
+  var margin = {top: 20, right: 30, bottom: 60, left: 120},
+  width = 350 - margin.left - margin.right,
+  height = 250 - margin.top - margin.bottom;
+  var svg = d3.select("#" + divname)
+
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -304,17 +336,43 @@ var y = d3.scaleBand()
 svg.append("g")
   .call(d3.axisLeft(y));
 
-
  //Bars
- svg.selectAll("myRect")
+//  var myColor = d3.scale()
+//   .domain([d3.min(data, d=>d[colorTheme]), d3.max(data, d=>d[colorTheme])])
+//   .interpolator(d3.interpolateBlues);
+console.log(data);
+ svg.selectAll("Rect")
     .data(data)
     .enter()
     .append("rect")
     .attr("x", x(0) )
     .attr("y", function(d) { return y(d.Model); })
     .attr("width", function(d) { return x(d[divname]); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2");
+    .attr("height", y.bandwidth() ) 
+    .attr("fill", function(d){return(d.Model=="bmw 2002" ? "#d53e4f":"#3288bd"); });
+
+    // svg.selectAll("circlebmw")
+    // .data(data.filter((d,i)=>{return d.Model=="bmw 2002"}))
+    // .enter().append("circle")
+    // .attr("cx", function(d){return(x(d.pca1));})
+    // .attr("cy", function(d) { return(y(d.pca2));})
+    // // .attr("width", function(d) { return x(d[divname]);})
+    // .attr("r", radius)
+    // .attr("fill", "red")
+    // .attr("fill-opacity", 1.0);
+
+  //  svg.selectAll("RectLabels")
+  //         .data(data)
+  //         .enter()
+  //         .append("text")
+  //         .text(d=>d[divname])
+  //         .attr("x", function(d) { return x(d[divname]);})
+  //         .attr("y", d => function(d) { return(x(d[divname])+y.bandwidth()/2);})
+  //         .attr("text-anchor", "middle")
+  //         ;
+
+//for others
+
         
   svg.append("text")
     .attr("x", (width / 2))             
@@ -322,7 +380,7 @@ svg.append("g")
     .attr("text-anchor", "middle")  
     .style("font-size", "16px") 
     .style("text-decoration", "underline")
-    .style('fill', 'blue')  
+    .style('fill', '#ff8544')  
     .text(divname);  
     
   d3.select("#choice1").on("change", function(d){
@@ -331,7 +389,7 @@ svg.append("g")
   })
 
   
-  };
+  }
 
 
 
@@ -370,24 +428,22 @@ renderScatterChart(AllData);
 // Once the user makes a choise, 
 // all graphs will be updated with that value
 
-d3.select("#choice1")
-  .selectAll('myOptions')
-  .data(AllData).enter()
-  .append("option")
-  .text(function(d){return(d.Model)})
-  .attr("value", function(d){return d;})
+// d3.select("#choice1")
+//   .selectAll('myOptions')
+//   .data(AllData).enter()
+//   .append("option")
+//   .text(function(d){return(d.Model)})
+//   .attr("value", function(d){return d;})
 
 
 
-
-
-renderBarChart(AllData.filter((d,i)=>{return i<6}), "Acceleration");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"MPG");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"Cylinders");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"Displacement");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"Horsepower");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"Weight");
-renderBarChart(AllData.filter((d,i)=>{return i<8}),"Year");
+// renderBarChart(AllData.filter((d,i)=>{return i<6}), "Acceleration");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"MPG");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"Cylinders");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"Displacement");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"Horsepower");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"Weight");
+// renderBarChart(AllData.filter((d,i)=>{return i<8}),"Year");
 
 
 // update();
